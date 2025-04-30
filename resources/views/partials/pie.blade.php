fe0dad2fb4fbb87e84dace4406396b2bf51054c3
cmonzo <carlesmm85@gmail.com>
2025-04-30 16:10:26 +0200

    30apr2025

--- resources/views/partials/pie.blade.php
+++ resources/views/partials/pie.blade.php
@@ -1,10 +1,10 @@
-<div class="card" style="width: 100%; height: 25%; background-color: #DE0124;">
+<div class="card" style="">
     <ul class="list-group list-group-flush">
-        <li class="list-group-item"><a href="{{route('quienes')}}">¿ Quienes somos?</a></li>
-        <li class="list-group-item"><a href="{{route('indice')}}"><img src="img/trademark.png" alt="trademark"></a></li>
+        <li class="list-group-item"><a href="{{route('quienes')}}">¿Quienes somos?</a></li>
+        <li class="list-group-item"><a href="{{route('indice')}}"><img src="/img/trademark.png" alt="trademark"></a></li>
         <li class="list-group-item">Desde 2020</li>
     </ul>
-    <div class="card-footer" style="background-color: #DE0124">
+    <div class="card-footer">
         WEB DESARROLLADA POR CARLOS MONZÓ
     </div>
 </div>
